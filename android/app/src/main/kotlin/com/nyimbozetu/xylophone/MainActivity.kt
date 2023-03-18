a3af478b2c42ef1a5051f687161747cddf4ba102
Musyimi <musyimi13@gmail.com>
2023-03-18 11:21:28 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/nyimbozetu/xylophone/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nyimbozetu.xylophone
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const XylophoneApp());
+}
+
+class XylophoneApp extends StatelessWidget {
+  const XylophoneApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: SafeArea(
+          child: Container(),
+        ),
+      ),
+    );
+  }
+}
